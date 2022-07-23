7e40a198239fbef8cee0c049db6283cd4a5238b6
Eric Mok <ericmok@Erics-MacBook-Air.local>
2022-07-23 03:26:46 -0400

    update

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -5,7 +5,7 @@ import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import {useQuery, useMutation} from '@apollo/client';
 import {GET_ME} from '../utils/queries';
-import {REMOVE_BOOK} from '../utils/queries';
+import {REMOVE_BOOK} from '../utils/mutations';
 
 const SavedBooks = () => {
   const {loading, data} = useQuery(GET_ME);
