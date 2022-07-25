ca077dcb0ad5870c2697feb8b5639f8d1a678cb9
Eric Mok <ericmok@Erics-MacBook-Air.local>
2022-07-25 09:59:24 -0400

    update code

--- server/server.js
+++ server/server.js
@@ -24,9 +24,9 @@ app.use(express.json());
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
-app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
+// app.get('/', (req, res)=>{
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// })
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
